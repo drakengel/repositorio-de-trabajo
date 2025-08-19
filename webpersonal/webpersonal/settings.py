9830fee88b7e2d27d6a137551ebce1bcb27e49ae
william <william.jesus.rcastillo.com>
2025-08-18 21:14:31 -0400

    commit

--- webpersonal/webpersonal/settings.py
+++ webpersonal/webpersonal/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',  # Your core app
+    'core', 
+    'portfolio', # Your core app
 ]
 
 MIDDLEWARE = [

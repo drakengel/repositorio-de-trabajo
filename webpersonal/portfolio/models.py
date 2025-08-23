89a2e52d400c3fb7b01155f786f94953e4bbd131
william <william.jesus.rcastillo.com>
2025-08-22 21:14:36 -0400

    commit

--- webpersonal/portfolio/models.py
+++ webpersonal/portfolio/models.py
@@ -7,6 +7,7 @@ class Portfolio(models.Model):
     image = models.ImageField(verbose_name="Imagen", upload_to="Projects")
     created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
     updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de actualización")
+    information = models.URLField( max_length=200, null=True, blank=True, verbose_name="Información adicional")
 
     class Meta:
         verbose_name = "Proyecto"
